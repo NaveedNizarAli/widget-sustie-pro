de48d50c1b6032b9467ee307429cbfb1a133d643
NaveedNizarAli <naveednizar9@gmail.com>
2023-04-11 20:35:43 +0500

    .

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -192,8 +192,8 @@ function () {
         fetch('https://sheetdb.io/api/v1/m95hi9ve95rqt').then(function (response) {
           return response.json();
         }).then(function (data) {
-          // let url = window.location.href;
-          var url = "https://nidarosfashion.com/products/easy-shorts-shorts-gap";
+          var url = window.location.href; // let url = "https://nidarosfashion.com/products/easy-shorts-shorts-gap";
+
           if (url.includes("?")) url = url.split("?")[0];
           console.log(url, data);
           var check = false;
@@ -307,7 +307,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50431" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49857" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
